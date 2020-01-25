9310d9fed8baeec3efde5c333cc604ae451d6007
McCabe Northup <mccabenorthup@gmail.com>
2020-01-25 15:55:40 -0700

    updated server to include inquirer, console.table and mysql

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
+    host: "localhost",
+    port: 3306,
+    user: "root",
 
-  // Your password
-  password: "Future@2020*",
-  database: "CMS_db"
+    // Your password
+    password: "Future@2020*",
+    database: "CMS_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    startCSM();
 });
+
+const startCSM = () => {
+    inquirer
+        .prompt([
+            {
+                type: "rawlist",
+                message: "What would you like to perform?",
+                choices: [
+                    "View all employees",
+                    "View All Employees by Department",
+                    "View All Employees by Manager",
+                    "Add Employee",
+                    "Remove Employee",
+                    "Update Employee Role",
+                    "Update Employee Manager",
+                    "Exit"
+                ],
+                name: "startAnswer"
+            }
+        ])
+        .then(answers => {
+            console.log(answers);
+            // switch (answer.action) {
+            //     case "View all employees":
+            //       employeeView();
+            //       break;
+        
+            //     case "View all departments":
+            //       departmentView();
+            //       break;          
+        });
+}
